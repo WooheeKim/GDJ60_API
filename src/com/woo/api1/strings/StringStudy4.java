a816a3a28555bdc45962d318644667e37ae36633
WooheeKim <kimdngml12@gmail.com>
2023-01-09 00:34:12 +0900

    1

---
+++ src/com/woo/api1/strings/StringStudy4.java
@@ -0,0 +1,24 @@
+package com.woo.api1.strings;
+
+public class StringStudy4 {
+
+	public static void main(String[] args) {
+		String str = "StarWars";
+		
+		String result = str.substring(4);
+		
+		System.out.println(result);
+		
+		result = str.substring(4, 6);
+		
+		System.out.println(result);
+		
+		str = "sdf@naver.com";
+		// int idx = str.indexOf("@");//4
+		result = str.substring(str.indexOf("@")+1);
+		
+		System.out.println(result);
+
+	}
+
+}
