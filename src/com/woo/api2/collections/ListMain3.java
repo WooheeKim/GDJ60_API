6d4c289ad1f06f9932c5e3408556905b99fa86c2
Kimwoohee <kimdngml12@gmail.com>
2023-01-09 17:24:06 +0900

    1

--- src/com/woo/api2/collections/ListMain3.java
+++ src/com/woo/api2/collections/ListMain3.java
@@ -1,9 +1,34 @@
 package com.woo.api2.collections;
 
+import java.util.ArrayList;
+
 public class ListMain3 {
 
 	public static void main(String[] args) {
+		// Collection은 Type에 안전하지 않다.
+		// Generic : Type의 안전성
+		// <E> : 변수명
+		ArrayList<Integer> ar = new ArrayList<>();
+		ar.add(1);
 		
+		int num = ar.get(0);
+		
+		ArrayList<WetherDTO> ar2 = new ArrayList<>();
+		WetherDTO wetherDTO = new WetherDTO();
+		wetherDTO.setCity("Seoul");
+		ar2.add(wetherDTO);
+		wetherDTO = new WetherDTO();
+		wetherDTO.setCity("Jeju");
+		ar2.add(wetherDTO);
+		
+//		wetherDTO = new WetherDTO();
+//		wetherDTO.setCity("Jeju");
+		
+		ar2.remove(wetherDTO);
+		
+		for(int i=0;i<ar2.size();i++) {
+			System.out.println(ar2.get(i).getCity());
+		}
 
 	}
 
