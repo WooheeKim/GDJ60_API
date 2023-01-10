680f6a38f1372be5d1a5ef626f1c617b764430e3
Kimwoohee <kimdngml12@gmail.com>
2023-01-10 11:04:03 +0900

    1

--- src/com/woo/api2/collections/ex1/StudentDAO.java
+++ src/com/woo/api2/collections/ex1/StudentDAO.java
@@ -1,5 +1,9 @@
 package com.woo.api2.collections.ex1;
 
+import java.util.StringTokenizer;
+
+import com.woo.api1.strings.ex3.WeatherDTO;
+
 public class StudentDAO {
 	
 	private StringBuffer sb;
@@ -12,6 +16,19 @@ public class StudentDAO {
 		sb.append("suji, 3, 89, 74, 87 ");
 		sb.append("choa, 4, 71, 25, 99 ");
 		
+	}
+	
+	public StudentDAO [] init() {
+		StringTokenizer str = new StringTokenizer(this.data,",");
+		StudentDAO [] students = new StudentDAO[str.countTokens()/4];
+		int i = 0;
+		while(str.hasMoreTokens()) {
+			StudentDAO studentDAO = new StudentDAO();
+			
+			students[i] = studentDAO;
+			i++;
+		}
+		return students;
 	}
 	
 }
