2301f66dcbfd16aa877028d0ffaa757d95d23014
Kimwoohee <kimdngml12@gmail.com>
2023-01-11 11:46:36 +0900

    1

--- src/com/woo/api2/data/CalendarMain4.java
+++ src/com/woo/api2/data/CalendarMain4.java
@@ -1,5 +1,6 @@
 package com.woo.api2.data;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class CalendarMain4 {
@@ -7,6 +8,11 @@ public class CalendarMain4 {
 	public static void main(String[] args) {
 		Calendar ca = Calendar.getInstance();
 		// 2023년 1월 11일 => format
+		SimpleDateFormat sd = new SimpleDateFormat("yyyy년 MM월 dd일 E요일");
+		String p = sd.format(ca.getTime());
+		
+		System.out.println(p);
+		
 
 	}
 
