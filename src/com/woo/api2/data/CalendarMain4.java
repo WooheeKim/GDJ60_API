8a06ce216e6c2c974102e323b168598e96962641
Kimwoohee <kimdngml12@gmail.com>
2023-01-11 12:05:19 +0900

    1

--- src/com/woo/api2/data/CalendarMain4.java
+++ src/com/woo/api2/data/CalendarMain4.java
@@ -8,7 +8,8 @@ public class CalendarMain4 {
 	public static void main(String[] args) {
 		Calendar ca = Calendar.getInstance();
 		// 2023년 1월 11일 => format
-		SimpleDateFormat sd = new SimpleDateFormat("yyyy년 MM월 dd일 E요일");
+		SimpleDateFormat sd = new SimpleDateFormat();
+		sd.applyPattern("yyyy년 MM월 dd일 E요일");
 		String p = sd.format(ca.getTime());
 		
 		System.out.println(p);
---
+++ src/com/woo/api2/data/UUIDMain.java
@@ -0,0 +1,14 @@
+package com.woo.api2.data;
+
+import java.util.UUID;
+
+public class UUIDMain {
+
+	public static void main(String[] args) {
+		
+		String name = UUID.randomUUID().toString();
+		System.out.println(name);
+		
+	}
+
+}
